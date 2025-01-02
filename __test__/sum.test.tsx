5fd79ed2d3ba8e038b98df97440293ad3172019e
scs0209 <tjdckdtn2463@naver.com>
2025-01-02 21:42:20 +0900

    feat: eslint 및 prettier 설정 완료

--- __test__/sum.test.tsx
+++ __test__/sum.test.tsx
@@ -4,14 +4,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from '@/app/page';
 
-
 test('renders Home component', () => {
   render(<Home />);
   const mainElement = screen.getByRole('main');
   expect(mainElement).toBeInTheDocument();
   expect(mainElement).toHaveTextContent('hi');
 }); 
-
-// test('adds 1 + 2 to equal 3', () => {
-//   expect((2)).toBe(3);
-// });
