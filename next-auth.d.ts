b03ded343531df33df25087d537119c3ad73e3a0
scs0209 <tjdckdtn2463@naver.com>
2025-01-20 23:04:25 +0900

    chore: next-auth 타입 확장

---
+++ next-auth.d.ts
@@ -0,0 +1,21 @@
+import type NextAuth from 'next-auth';
+import { DefaultSession, DefaultUser } from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+      accessToken?: string;
+      role: string;
+      address: string;
+      detail_address: string;
+      image?: string;
+    } & DefaultSession['user'];
+  }
+
+  interface User extends DefaultUser {
+    role?: Role | null;
+    address?: string;
+    detail_address?: string;
+  }
+}
