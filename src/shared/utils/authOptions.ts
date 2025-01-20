5b6f18f50c558c9b89a0ea2a6d53a3c90208751b
scs0209 <tjdckdtn2463@naver.com>
2025-01-20 23:03:24 +0900

    feat: next-auth option 코드 완성

---
+++ src/shared/utils/authOptions.ts
@@ -0,0 +1,84 @@
+import { NextAuthConfig } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { compare } from 'bcrypt';
+import prisma from '../lib/db';
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      /* @ts-ignore */
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error('Please enter an email and password');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user?.password) {
+          throw new Error('No user found');
+        }
+
+        const isValid = await compare(credentials.password, user.password);
+
+        if (!isValid) {
+          throw new Error('Invalid password');
+        }
+
+        if (user) {
+          return user;
+        }
+        return null;
+      },
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (user) {
+        // OAuth 로그인 시 로직
+        return {
+          ...token,
+          id: account?.id ? account.id : user.id,
+          accessToken: account?.access_token,
+          role: user.role,
+          address: user.address,
+          detail_address: user.detail_address,
+        };
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          name: token.name,
+          accessToken: token.accessToken ? token.accessToken : null,
+          role: token.role,
+          address: token.address,
+          detail_address: token.detail_address,
+        },
+      };
+    },
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  debug: process.env.NODE_ENV === 'development',
+} satisfies NextAuthConfig;
