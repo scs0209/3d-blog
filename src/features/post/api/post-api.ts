d8b495e53fd241d209eaec281d11a4aed67df507
scs0209 <tjdckdtn2463@naver.com>
2025-02-23 21:09:17 +0900

    chore: 게시물 리스트 불러오는 api 추가

--- src/features/post/api/post-api.ts
+++ src/features/post/api/post-api.ts
@@ -1,6 +1,10 @@
 import { Post } from '@prisma/client';
 import { getSession } from 'next-auth/react';
 import { ApiRequest, fetcher } from '@/shared/api';
+import { GetPostListResponse } from '../model/post-types';
+
+export const getPostList = (): GetPostListResponse =>
+  fetcher({ url: '/api/posts', method: 'get' });
 
 type CreatePostInput = {
   title: string;
