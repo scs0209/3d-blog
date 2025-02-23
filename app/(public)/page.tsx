aaf7529e773e6780655f1deb6bd9e1691ef3bf99
scs0209 <tjdckdtn2463@naver.com>
2025-02-23 21:10:53 +0900

    feat: 메인 페이지에 블로그 헤더, 메인, 푸터 적용

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { auth } from '@/shared/utils/auth';
 import SignoutButton from '@/features/auth/ui/signout-button';
+import { BlogFooter, BlogHeader, BlogMain } from '@/widgets/home';
 
 export default async function Home() {
   const session = await auth();
 
   return (
     <main className="flex flex-col items-center row-start-2 gap-8 sm:items-start">
+      <BlogHeader />
+      <BlogMain />
       <div>{session?.user.name}</div>
       <SignoutButton />
+      <BlogFooter />
     </main>
   );
 }
---
+++ src/widgets/home/index.ts
@@ -0,0 +1,3 @@
+export { BlogHeader } from './ui/header';
+export { BlogMain } from './ui/blog-main';
+export { BlogFooter } from './ui/blog-footer';
