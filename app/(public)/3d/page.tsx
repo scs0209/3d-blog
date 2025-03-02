d805f3740c0e0e506c637ca2456cf8d1e5cf9f57
scs0209 <tjdckdtn2463@naver.com>
2025-03-02 19:33:16 +0900

    feat: 큐브맵 추가 및 3d 페이지에 적용

--- app/(public)/3d/page.tsx
+++ app/(public)/3d/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Model as RoomModel } from '@/shared/ui/Room';
 import { CameraLogger } from '@/shared/ui/CameraLogger';
+import SkyBox from '@/shared/ui/SkyBox';
+import CanvasLoader from '@/shared/ui/Loader';
 
 const GlTFPage = () => {
   return (
@@ -13,9 +15,12 @@ const GlTFPage = () => {
         camera={{ fov: 75, near: 0.1, far: 1000, position: [12, 238, -410] }}
       >
         <ambientLight intensity={2} color="white" />
-        <RoomModel position={[0, -100, 0]} />
-        <OrbitControls />
-        <CameraLogger />
+        <Suspense fallback={<CanvasLoader />}>
+          <SkyBox />
+          <RoomModel position={[0, -100, 0]} />
+          <OrbitControls />
+          <CameraLogger />
+        </Suspense>
       </Canvas>
     </div>
   );
