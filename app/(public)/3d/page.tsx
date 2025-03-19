552c0e26c7b827a1ad75563f477cfc2a72b2273f
scs0209 <tjdckdtn2463@naver.com>
2025-03-19 23:00:52 +0900

    chore: 3D 씬에서 Cloud 컴포넌트 및 안개 효과 제거

--- app/(public)/3d/page.tsx
+++ app/(public)/3d/page.tsx
@@ -10,7 +10,6 @@ import { CubeModel } from '@/shared/ui/Cube';
 // import SkyBox from '@/shared/ui/SkyBox';
 import { Model } from '@/shared/ui/Scene';
 import CanvasLoader from '@/shared/ui/Loader';
-import { Cloud } from '@/shared/ui/Cloud';
 
 const CameraController = () => {
   const { camera } = useThree();
@@ -68,10 +67,7 @@ const GlTFPage = () => {
           {/* <CubeModel /> */}
 
           <OrbitControls />
-          <fog attach="fog" args={['#202025', 0, 80]} />
-          <Cloud count={8} radius={20} />
-          {/* <group rotation={[10, 10.5, 10]} scale={0.1}>
-          </group> */}
+
           <CameraLogger />
         </Suspense>
       </Canvas>
