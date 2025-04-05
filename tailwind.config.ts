fda29169f9eb72cc18448ebf8162bafef6c225e8
scs0209 <tjdckdtn2463@naver.com>
2025-04-05 21:15:28 +0900

    style: Tailwind 설정에 위젯 경로 추가 및 새로운 애니메이션 효과인 그라디언트 테두리 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ export default {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/shadcn-ui/**/*.{js,ts,jsx,tsx}',
     './src/shared/ui/**/*.{js,ts,jsx,tsx}',
+    './src/widgets/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -71,6 +72,7 @@ export default {
       },
       animation: {
         'meteor-effect': 'meteor 5s linear infinite',
+        'gradient-border': 'gradientBorder 3s linear infinite',
       },
       keyframes: {
         meteor: {
@@ -79,6 +81,20 @@ export default {
           '100%': {
             transform: 'rotate(215deg) translateX(-500px)',
             opacity: '0',
+          },
+        },
+        gradientBorder: {
+          '0%, 100%': {
+            borderImage:
+              'linear-gradient(0deg, rgba(255,128,0,0.7), rgba(255,255,0,0.7), rgba(0,255,0,0.7), rgba(0,255,255,0.7), rgba(0,0,255,0.7), rgba(128,0,255,0.7), rgba(255,0,128,0.7)) 1',
+            boxShadow:
+              '0 0 5px rgba(255,0,0,0.2), 0 0 10px rgba(0,255,255,0.2), 0 0 15px rgba(255,0,255,0.2)',
+          },
+          '50%': {
+            borderImage:
+              'linear-gradient(180deg, rgba(255,128,0,0.7), rgba(255,255,0,0.7), rgba(0,255,0,0.7), rgba(0,255,255,0.7), rgba(0,0,255,0.7), rgba(128,0,255,0.7), rgba(255,0,128,0.7)) 1',
+            boxShadow:
+              '0 0 10px rgba(255,0,0,0.3), 0 0 20px rgba(0,255,255,0.3), 0 0 30px rgba(255,0,255,0.3)',
           },
         },
       },
