92d352b694e8bdc785f1f71ff3960fe380c3f306
scs0209 <tjdckdtn2463@naver.com>
2025-04-06 20:18:06 +0900

    style: Scene.tsx에서 새로운 재질 추가 및 구조 개선

--- src/shared/ui/Scene.tsx
+++ src/shared/ui/Scene.tsx
@@ -7,7 +7,7 @@ Title: space boi
 */
 
 import React, { useState } from 'react';
-import { useGLTF, Float, TrackballControls } from '@react-three/drei';
+import { useGLTF, Float } from '@react-three/drei';
 import { MeshStandardMaterial } from 'three';
 import * as THREE from 'three';
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
@@ -18,10 +18,19 @@ import { NeuralNetwork } from './NeutralNetwork';
 import { Cloud } from './Cloud';
 
 const mirrorMaterial = new MeshStandardMaterial({
-  color: 'white', // 색상 (투명도가 있음)
-  transparent: true, // 투명 활성화
-  opacity: 0.2, // 투명도 (0 = 완전 투명, 1 = 불투명)
-  depthWrite: false, // 깊이 버퍼에 쓰지 않음 (겹칠 때 문제 방지)
+  color: 'white',
+  transparent: true,
+  opacity: 0.2,
+  depthWrite: false,
+});
+
+const spaceMaterial = new MeshStandardMaterial({
+  color: '#000011',
+  roughness: 0.2,
+  transparent: true,
+  opacity: 0.4,
+  emissive: '#000033',
+  emissiveIntensity: 0.2,
 });
 
 export function Model(props: React.ComponentProps<'group'>) {
@@ -34,201 +43,204 @@ export function Model(props: React.ComponentProps<'group'>) {
   };
 
   return (
-    <group {...props} dispose={null}>
-      <group scale={0.01}>
-        <group rotation={[-Math.PI / 2, 0, 0]} scale={1}>
-          <RoomModel position={[0, 0, 20]} />
-          <CubeModel position={[0, -50, 300]} onHover={handleCubeHover} />
-          <NeuralNetwork />
-          {/* <group rotation={[10, 10.5, 10]} scale={30}>
-            <fog attach="fog" args={['#202025', 0, 80]} />
+    <>
+      <fog attach="fog" args={['#000022', 0, 3000]} />
+      <group {...props} dispose={null}>
+        <group scale={0.01}>
+          <group rotation={[-Math.PI / 2, 0, 0]} scale={1}>
+            <RoomModel position={[0, 0, 20]} />
+            <CubeModel position={[0, -50, 300]} onHover={handleCubeHover} />
+            <NeuralNetwork />
+            {/* <group rotation={[10, 10.5, 10]} scale={30}>
+              <fog attach="fog" args={['#202025', 0, 80]} />
 
-            <Cloud />
-          </group> */}
-          {/* <TrackballControls /> */}
-          {isCubeHovered && (
-            <>
-              <Float speed={4} rotationIntensity={1} floatIntensity={2}>
-                <Atom />
-              </Float>
-              <EffectComposer>
-                <Bloom mipmapBlur luminanceThreshold={1} radius={0.7} />
-              </EffectComposer>
-            </>
-          )}
-        </group>
-        <group
-          position={[-357.404, 392.646, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={39.706}
-        >
+              <Cloud />
+            </group> */}
+            {/* <TrackballControls /> */}
+            {isCubeHovered && (
+              <>
+                <Float speed={4} rotationIntensity={1} floatIntensity={2}>
+                  <Atom />
+                </Float>
+                <EffectComposer>
+                  <Bloom mipmapBlur luminanceThreshold={1} radius={0.7} />
+                </EffectComposer>
+              </>
+            )}
+          </group>
+          <group
+            position={[-357.404, 392.646, 0]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={39.706}
+          >
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={(nodes.Sphere002_Material001_0 as THREE.Mesh).geometry}
+              material={materials['Material.001']}
+            />
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={(nodes.Sphere002_Material002_0 as THREE.Mesh).geometry}
+              material={materials['Material.002']}
+            />
+          </group>
+          <group
+            position={[199.634, 566.883, -221.001]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={39.706}
+          >
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={(nodes.Sphere007_Material001_0 as THREE.Mesh).geometry}
+              material={materials['Material.001']}
+            />
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={(nodes.Sphere007_Material002_0 as THREE.Mesh).geometry}
+              material={materials['Material.002']}
+            />
+          </group>
           <mesh
             castShadow
             receiveShadow
-            geometry={(nodes.Sphere002_Material001_0 as THREE.Mesh).geometry}
-            material={materials['Material.001']}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={(nodes.Sphere002_Material002_0 as THREE.Mesh).geometry}
+            geometry={(nodes.waves_Material002_0 as THREE.Mesh).geometry}
             material={materials['Material.002']}
-          />
-        </group>
-        <group
-          position={[199.634, 566.883, -221.001]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={39.706}
-        >
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={(nodes.Sphere007_Material001_0 as THREE.Mesh).geometry}
-            material={materials['Material.001']}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={[100, 100, 1.891]}
           />
           <mesh
             castShadow
             receiveShadow
-            geometry={(nodes.Sphere007_Material002_0 as THREE.Mesh).geometry}
+            geometry={(nodes.waves1_Material002_0 as THREE.Mesh).geometry}
             material={materials['Material.002']}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={[100, 100, 1.891]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.waves2_Material002_0 as THREE.Mesh).geometry}
+            material={materials['Material.002']}
+            position={[92.464, 15.529, 2.112]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={[100, 100, 1.891]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.particles_Material002_0 as THREE.Mesh).geometry}
+            material={materials['Material.002']}
+            position={[489.69, 793.811, 355.293]}
+            rotation={[-Math.PI / 2, 0, -Math.PI / 2]}
+            scale={20.408}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.Sphere_Material001_0 as THREE.Mesh).geometry}
+            material={materials['Material.001']}
+            position={[375.469, 427.948, 0]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={62.402}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.Sphere001_Material002_0 as THREE.Mesh).geometry}
+            material={materials['Material.002']}
+            position={[375.469, 427.948, 0]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={60.324}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.Sphere004_Material002_0 as THREE.Mesh).geometry}
+            material={materials['Material.002']}
+            position={[375.469, 427.948, 0]}
+            rotation={[-0.688, 0, 0]}
+            scale={[104.129, 81.609, 0]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.Sphere005_Material001_0 as THREE.Mesh).geometry}
+            material={materials['Material.001']}
+            position={[-341.988, 460.196, -117.028]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={62.402}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.Sphere006_Material002_0 as THREE.Mesh).geometry}
+            material={materials['Material.002']}
+            position={[-341.988, 460.196, -117.028]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={60.324}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.Sphere009_Material002_0 as THREE.Mesh).geometry}
+            material={materials['Material.002']}
+            position={[507.522, 667.594, -214.475]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={16.881}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.Sphere010_Material002_0 as THREE.Mesh).geometry}
+            material={materials['Material.002']}
+            position={[-287.442, 585.792, -311.857]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={16.881}
+          />
+          {/* 바닥 */}
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.Sphere011_Material002_0 as THREE.Mesh).geometry}
+            material={mirrorMaterial}
+            position={[-553.462, 331.074, -379.067]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={11.437}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.Cube_Material001_0 as THREE.Mesh).geometry}
+            material={spaceMaterial}
+            position={[0, -101.673, 0]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={[50000, 50000, 100]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.Sphere003_Material002_0 as THREE.Mesh).geometry}
+            material={materials['Material.002']}
+            position={[-357.404, 392.646, 0]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={41.075}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={(nodes.Sphere008_Material002_0 as THREE.Mesh).geometry}
+            material={materials['Material.002']}
+            position={[199.634, 566.883, -221.001]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={41.075}
           />
         </group>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.waves_Material002_0 as THREE.Mesh).geometry}
-          material={materials['Material.002']}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={[100, 100, 1.891]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.waves1_Material002_0 as THREE.Mesh).geometry}
-          material={materials['Material.002']}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={[100, 100, 1.891]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.waves2_Material002_0 as THREE.Mesh).geometry}
-          material={materials['Material.002']}
-          position={[92.464, 15.529, 2.112]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={[100, 100, 1.891]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.particles_Material002_0 as THREE.Mesh).geometry}
-          material={materials['Material.002']}
-          position={[489.69, 793.811, 355.293]}
-          rotation={[-Math.PI / 2, 0, -Math.PI / 2]}
-          scale={20.408}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.Sphere_Material001_0 as THREE.Mesh).geometry}
-          material={materials['Material.001']}
-          position={[375.469, 427.948, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={62.402}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.Sphere001_Material002_0 as THREE.Mesh).geometry}
-          material={materials['Material.002']}
-          position={[375.469, 427.948, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={60.324}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.Sphere004_Material002_0 as THREE.Mesh).geometry}
-          material={materials['Material.002']}
-          position={[375.469, 427.948, 0]}
-          rotation={[-0.688, 0, 0]}
-          scale={[104.129, 81.609, 0]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.Sphere005_Material001_0 as THREE.Mesh).geometry}
-          material={materials['Material.001']}
-          position={[-341.988, 460.196, -117.028]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={62.402}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.Sphere006_Material002_0 as THREE.Mesh).geometry}
-          material={materials['Material.002']}
-          position={[-341.988, 460.196, -117.028]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={60.324}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.Sphere009_Material002_0 as THREE.Mesh).geometry}
-          material={materials['Material.002']}
-          position={[507.522, 667.594, -214.475]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={16.881}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.Sphere010_Material002_0 as THREE.Mesh).geometry}
-          material={materials['Material.002']}
-          position={[-287.442, 585.792, -311.857]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={16.881}
-        />
-        {/* 바닥 */}
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.Sphere011_Material002_0 as THREE.Mesh).geometry}
-          material={mirrorMaterial}
-          position={[-553.462, 331.074, -379.067]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={11.437}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.Cube_Material001_0 as THREE.Mesh).geometry}
-          material={mirrorMaterial}
-          position={[0, -101.673, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={[1120.013, 1120.013, 100]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.Sphere003_Material002_0 as THREE.Mesh).geometry}
-          material={materials['Material.002']}
-          position={[-357.404, 392.646, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={41.075}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={(nodes.Sphere008_Material002_0 as THREE.Mesh).geometry}
-          material={materials['Material.002']}
-          position={[199.634, 566.883, -221.001]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={41.075}
-        />
       </group>
-    </group>
+    </>
   );
 }
 
