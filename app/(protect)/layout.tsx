ff7915867510d4c104e86cc17875fcf03d2ed486
scs0209 <tjdckdtn2463@naver.com>
2025-04-13 18:18:32 +0900

    chore: layout.tsx 파일에서 코드 스타일 통일을 위한 포맷팅 수정

--- app/(protect)/layout.tsx
+++ app/(protect)/layout.tsx
@@ -1,15 +1,12 @@
 import type React from 'react';
-import {
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/shadcn-ui/components/ui/sidebar';
+import { SidebarProvider, SidebarTrigger } from '@/shadcn-ui/components/ui/sidebar';
 import AuthSidebar from '@/shared/ui/AuthSidebar';
 
 const ProtectLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <SidebarProvider>
       <AuthSidebar />
-      <main className="p-4 protect">
+      <main className='p-4 protect'>
         <SidebarTrigger />
         {children}
       </main>
