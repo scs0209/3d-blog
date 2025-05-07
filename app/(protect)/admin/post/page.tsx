a3d5623dac8ecc4f9f09f16958a0279d29461ca3
ayaan <ayaan.sung@shoplworks.com>
2025-05-07 14:12:39 +0900

    fix: 블로그 포스트 작성 페이지에서 포스트 생성 핸들러 및 폼 컴포넌트 주석 처리

--- app/(protect)/admin/post/page.tsx
+++ app/(protect)/admin/post/page.tsx
@@ -1,27 +1,28 @@
+// bcrypt를 사용하기 위해서는 서버 컴포넌트로 동작해야됨
 import { createPost } from '@/features/post/api/post-api';
 import type { PostFormSchema } from '@/features/post/model/post-form-schema';
 import PostForm from '@/features/post/ui/post-form';
 import { auth } from '@/shared/utils/auth';
 
 export default function NewBlogPostPage() {
-  const handleCreatePost = async (data: PostFormSchema) => {
-    try {
-      const session = await auth();
-      await createPost({
-        ...data,
-        authorId: Number(session?.user?.id) || 0,
-        categoryId: Number(data.categoryId),
-      });
-      alert('성공!');
-    } catch (error) {
-      console.error('Failed to create blog post:', error);
-    }
-  };
+  // const handleCreatePost = async (data: PostFormSchema) => {
+  //   try {
+  //     const session = await auth();
+  //     await createPost({
+  //       ...data,
+  //       authorId: Number(session?.user?.id) || 0,
+  //       categoryId: Number(data.categoryId),
+  //     });
+  //     alert('성공!');
+  //   } catch (error) {
+  //     console.error('Failed to create blog post:', error);
+  //   }
+  // };
 
   return (
     <div className='py-8 mx-auto'>
       <h1 className='mb-4 text-2xl font-bold'>새 블로그 포스트 작성</h1>
-      <PostForm onSubmit={handleCreatePost} />
+      {/* <PostForm onSubmit={handleCreatePost} /> */}
     </div>
   );
 }
