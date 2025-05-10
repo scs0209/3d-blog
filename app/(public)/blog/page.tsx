d421865664854e996ec9a9b8b66a304ad3006a50
scs0209 <tjdckdtn2463@naver.com>
2025-05-10 20:46:03 +0900

    feat: Blog 페이지에 3D Canvas 추가 및 Macintosh 컴포넌트 렌더링 설정

--- app/(public)/blog/page.tsx
+++ app/(public)/blog/page.tsx
@@ -1,17 +1,38 @@
-import { auth } from '@/shared/utils/auth';
-import SignoutButton from '@/features/auth/ui/signout-button';
-import { BlogFooter, BlogHeader, BlogMain } from '@/widgets/home';
+'use client';
 
-export default async function Home() {
-  const session = await auth();
+import { Macintosh } from '@/widgets/post/ui';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+export default function Home() {
+  // const session = await auth();
 
   return (
-    <main className='flex flex-col items-center row-start-2 gap-8 sm:items-start'>
-      <BlogHeader />
+    <main className='flex flex-col items-center row-start-2 gap-8 sm:items-start h-screen'>
+      <Canvas
+        camera={{ fov: 75, near: 0.1, zoom: 10, position: [0, 0, 10] }}
+        // onPointerMissed={() => setActiveObject(null)}
+      >
+        {/* 부드러운 전체 조명 */}
+        <ambientLight intensity={0.5} />
+        {/* 강한 방향성 조명 (그림자와 하이라이트) */}
+        <directionalLight
+          intensity={1.2}
+          position={[5, 10, 7]}
+          castShadow
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+        />
+        {/* 보조 조명 (입체감 보강) */}
+        <pointLight intensity={0.5} position={[-10, 5, -10]} />
+        <Macintosh />
+        <OrbitControls />
+      </Canvas>
+      {/* <BlogHeader />
       <BlogMain />
       <div>{session?.user.name}</div>
       <SignoutButton />
-      <BlogFooter />
+      <BlogFooter /> */}
     </main>
   );
 }
