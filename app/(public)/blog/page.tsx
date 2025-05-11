048ae8bd41e23a81cf3162e9673bff87bfc076e5
scs0209 <tjdckdtn2463@naver.com>
2025-05-11 20:06:57 +0900

    feat: Blog 페이지의 카메라 애니메이션 및 UI 요소 개선, 새로운 컴포넌트 추가

--- app/(public)/blog/page.tsx
+++ app/(public)/blog/page.tsx
@@ -1,15 +1,14 @@
 'use client';
 
-import { Macintosh } from '@/widgets/post/ui';
-import BlogContent from '@/widgets/post/ui/BlogContent';
+import { Macintosh, BlogContent, ComputerBackground } from '@/widgets/post/ui';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { useScroll, useTransform } from 'framer-motion';
+import { useScroll, useTransform, useMotionValueEvent } from 'framer-motion';
 import { useState, useEffect } from 'react';
 
-function AnimatedCamera({ cameraZ }: { cameraZ: any }) {
+function AnimatedCamera({ cameraPos }: { cameraPos: { x: number; y: number; z: number } }) {
   const { camera } = useThree();
   useFrame(() => {
-    camera.position.z = cameraZ.get();
+    camera.position.set(cameraPos.x, cameraPos.y, cameraPos.z);
     camera.updateProjectionMatrix();
   });
   return null;
@@ -17,10 +16,15 @@ function AnimatedCamera({ cameraZ }: { cameraZ: any }) {
 
 export default function BlogPage() {
   const { scrollYProgress } = useScroll();
-  const cameraZ = useTransform(scrollYProgress, [0, 1], [10, 2]);
-  const htmlScale = useTransform(scrollYProgress, [0.7, 1], [1, 2]);
-  const htmlOpacity = useTransform(scrollYProgress, [0.95, 1], [1, 0]);
+  // const cameraZ = useTransform(scrollYProgress, [0, 1], [10, 2]);
+  const htmlScaleMotion = useTransform(scrollYProgress, [0.7, 1], [1, 4]);
+  const htmlOpacityMotion = useTransform(scrollYProgress, [0.95, 1], [1, 0]);
+  const [htmlScale, setHtmlScale] = useState(1);
+  const [htmlOpacity, setHtmlOpacity] = useState(1);
   const [showFullPage, setShowFullPage] = useState(false);
+
+  useMotionValueEvent(htmlScaleMotion, 'change', setHtmlScale);
+  useMotionValueEvent(htmlOpacityMotion, 'change', setHtmlOpacity);
 
   useEffect(() => {
     const unsubscribe = scrollYProgress.on('change', (v) => {
@@ -34,21 +38,26 @@ export default function BlogPage() {
   }, [scrollYProgress]);
 
   return (
-    <main className='flex flex-col items-center row-start-2 gap-8 sm:items-start h-screen' style={{ height: '150vh' }}>
+    <main className='flex flex-col items-center row-start-2 gap-8 sm:items-start h-[150vh] bg-gray-700'>
       {!showFullPage && (
-        <Canvas camera={{ fov: 75, near: 0.1, zoom: 10, position: [0, 0, 10] }}>
-          <ambientLight intensity={0.5} />
-          <directionalLight
-            intensity={1.2}
-            position={[5, 10, 7]}
-            castShadow
-            shadow-mapSize-width={1024}
-            shadow-mapSize-height={1024}
-          />
-          <pointLight intensity={0.5} position={[-10, 5, -10]} />
-          <Macintosh htmlScale={htmlScale} htmlOpacity={htmlOpacity} />
-          <AnimatedCamera cameraZ={cameraZ} />
-        </Canvas>
+        <div className='fixed inset-0 z-10 w-full h-full'>
+          <Canvas camera={{ fov: 90, near: 1, zoom: 15, position: [-0.16, 1.4, 8.88] }}>
+            <ambientLight intensity={0.5} />
+            <directionalLight
+              intensity={1.2}
+              position={[5, 10, 7]}
+              castShadow
+              shadow-mapSize-width={1024}
+              shadow-mapSize-height={1024}
+            />
+            <pointLight intensity={0.5} position={[-10, 5, -10]} />
+            <Macintosh scale={0.3} htmlScale={htmlScale} htmlOpacity={htmlOpacity} position={[0, 0.1, 1]} />
+            <ComputerBackground scale={0.11} position={[0, 0.001, 0]} />
+            {/* <LevaCameraController /> */}
+            <AnimatedCamera cameraPos={{ x: -0.16, y: 1.4, z: 7.5 }} />
+            {/* <OrbitControls /> */}
+          </Canvas>
+        </div>
       )}
       {showFullPage && (
         <div className='fixed inset-0 z-50 bg-white flex items-center justify-center'>
