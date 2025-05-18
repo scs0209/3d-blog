cf1071adb0f587221675d0a9dd89e45072532c3a
scs0209 <tjdckdtn2463@naver.com>
2025-05-18 17:24:22 +0900

    feat: BlogPage에 조명 및 바닥 메쉬 추가, 포스트 프로세싱 효과 개선

--- app/(public)/blog/page.tsx
+++ app/(public)/blog/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { Macintosh, ComputerBackground, BlogMainPage } from '@/widgets/post/ui';
+import { Macintosh, BlogMainPage, ComputerBackground } from '@/widgets/post/ui';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { useState, useEffect, Suspense } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { Bloom, EffectComposer } from '@react-three/postprocessing';
+import { Bloom, DepthOfField, EffectComposer } from '@react-three/postprocessing';
 import { CanvasLoader } from '@/shared/ui';
+import { MeshReflectorMaterial } from '@react-three/drei';
 
 function AnimatedCamera({ cameraPos }: { cameraPos: { x: number; y: number; z: number } }) {
   const { camera } = useThree();
@@ -109,33 +110,52 @@ export default function BlogPage() {
             transition={{ duration: 0.6, ease: 'easeInOut' }}
             className='w-full h-full'
           >
-            <Canvas dpr={[1, 1.5]} camera={{ fov: 70, near: 1, zoom: 15, position: [-0.2, 1.5, 8.88] }}>
-              <ambientLight intensity={0.5} />
-              <directionalLight
-                intensity={1.2}
-                position={[5, 10, 7]}
+            <Canvas
+              dpr={[1, 1.5]}
+              camera={{ fov: 70, near: 1, zoom: 15, position: [-0.2, 1.5, 8.88] }}
+              eventPrefix='client'
+            >
+              <color attach='background' args={['black']} />
+              <hemisphereLight intensity={0.15} groundColor='black' />
+              <spotLight
+                decay={0}
+                position={[10, 20, 10]}
+                angle={0.12}
+                penumbra={1}
+                intensity={1}
                 castShadow
-                shadow-mapSize-width={1024}
-                shadow-mapSize-height={1024}
+                shadow-mapSize={1024}
               />
-              <pointLight intensity={0.5} position={[-10, 5, -10]} />
               <Suspense fallback={<CanvasLoader />}>
                 <Macintosh
                   scale={0.3}
                   htmlScale={htmlScale}
                   htmlOpacity={htmlOpacity}
                   showFullPage={showFullPage}
-                  position={[0, 0.1, 1]}
+                  position={[0, 0.1, 0.8]}
                 />
                 <ComputerBackground scale={0.11} position={[0, 0.001, 0]} />
                 {/* <LevaCameraController /> */}
                 <AnimatedCamera cameraPos={{ x: -0.16, y: 1.4, z: 7.5 }} />
-                <EffectComposer>
-                  <Bloom
-                    intensity={1.5} // 블룸 효과의 강도
-                    threshold={0.5} // 블룸 효과가 적용되는 임계치
-                    levels={10} // 블룸 효과의 레벨 수
+                {/* 바닥 */}
+                <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
+                  <planeGeometry args={[50, 50]} />
+                  <MeshReflectorMaterial
+                    blur={[300, 30]}
+                    resolution={2048}
+                    mixBlur={1}
+                    mixStrength={180}
+                    roughness={1}
+                    depthScale={1.2}
+                    minDepthThreshold={0.4}
+                    maxDepthThreshold={1.4}
+                    color='#202020'
+                    metalness={0.8}
                   />
+                </mesh>
+                <EffectComposer enableNormalPass={false}>
+                  <Bloom luminanceThreshold={0} mipmapBlur luminanceSmoothing={0.0} intensity={5} />
+                  <DepthOfField target={[0, 0, 13]} focalLength={0.3} bokehScale={15} height={700} />
                 </EffectComposer>
               </Suspense>
               {/* <OrbitControls /> */}
