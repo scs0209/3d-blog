433bc7dd464df4c909e590149f9e854c19b8036a
scs0209 <tjdckdtn2463@naver.com>
2025-05-31 23:30:21 +0900

    chore: 블로그 헤더에 검색 아이콘에 대한 제목 추가

--- src/widgets/home/ui/header.tsx
+++ src/widgets/home/ui/header.tsx
@@ -52,6 +52,7 @@ export function BlogHeader() {
                   viewBox='0 0 24 24'
                   stroke='currentColor'
                 >
+                  <title>Search</title>
                   <path d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
                 </svg>
               </div>
@@ -114,6 +115,7 @@ export function BlogHeader() {
                   viewBox='0 0 24 24'
                   stroke='currentColor'
                 >
+                  <title>Search</title>
                   <path d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
                 </svg>
               </div>
