3d13cea383b0aa0ab68fb4ae1a32bbb6c24b005c
scs0209 <tjdckdtn2463@naver.com>
2025-05-31 17:40:28 +0900

    feat: Vitest 설정 추가 및 sum 함수에 대한 테스트 케이스 작성, Button 컴포넌트 테스트 구조 변경

--- __test__/sum.test.tsx
+++ __test__/sum.test.tsx
@@ -1,9 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import Home from '../app/(public)/page';
+import { describe, it, expect } from 'vitest';
 
-test('renders Home component', () => {
-  render(<Home />);
-  const mainElement = screen.getByRole('main');
-  expect(mainElement).toBeInTheDocument();
-  expect(mainElement).toHaveTextContent('hi');
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+describe('sum function', () => {
+  it('adds two positive numbers correctly', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  it('adds a positive and a negative number correctly', () => {
+    expect(sum(5, -3)).toBe(2);
+  });
+
+  it('adds two negative numbers correctly', () => {
+    expect(sum(-1, -2)).toBe(-3);
+  });
+
+  it('adds zero correctly', () => {
+    expect(sum(0, 5)).toBe(5);
+    expect(sum(5, 0)).toBe(5);
+    expect(sum(0, 0)).toBe(0);
+  });
 });
--- src/shared/ui/Button/test/Button.test.tsx
+++ src/shared/ui/Button/test/Button.test.tsx
@@ -1,8 +1,11 @@
+import { describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Button from '../Button';
 
-test('renders Button component', () => {
-  render(<Button />);
-  const buttonElement = screen.getByText(/Post/i);
-  expect(buttonElement).toBeInTheDocument();
+describe('Button', () => {
+  test('renders Button component', () => {
+    render(<Button />);
+    const buttonElement = screen.getByText(/Post/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
