29614e067e2d011596fd4844c93bd60fd6086772
scs0209 <tjdckdtn2463@naver.com>
2025-05-31 18:49:27 +0900

    fix: 패키지 의존성 변경: bcrypt에서 bcryptjs로 변경 및 @types/bcrypt 제거, @types/bcryptjs 추가
    - 배포 에러 수정

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import prisma from '@/shared/lib/db';
 
 type RequestBody = {
--- src/shared/utils/auth.ts
+++ src/shared/utils/auth.ts
@@ -1,5 +1,5 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { compare } from 'bcrypt';
+import { compare } from 'bcryptjs';
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
