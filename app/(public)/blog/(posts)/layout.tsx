19419e2b66e26eb8b5fe9de30f0838e765f1735f
scs0209 <tjdckdtn2463@naver.com>
2025-06-07 16:24:13 +0900

    refactor: 블로그 포스트 레이아웃 컴포넌트 추가 및 구조 설정

---
+++ app/(public)/blog/(posts)/layout.tsx
@@ -0,0 +1,35 @@
+import { SpaceBackground } from '@/widgets/post/ui';
+import dynamic from 'next/dynamic';
+
+const BlogHeader = dynamic(() => import('@/widgets/post/ui/BlogHeader'));
+const BlogSidebarClient = dynamic(() => import('@/widgets/post/ui/BlogSidebarClient'));
+const BackToMainButton = dynamic(() => import('@/shared/ui/BackToMainButton'));
+
+export default function PostsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className='flex flex-col items-center row-start-2 gap-8 sm:items-start bg-white min-h-screen'>
+      <BackToMainButton />
+
+      <div className='w-full h-screen overflow-hidden bg-gradient-to-b from-[#181c2a] via-[#232946] to-[#23234d] text-slate-100 font-mono relative'>
+        <SpaceBackground />
+
+        <div className='flex h-screen'>
+          {/* Main Content (왼쪽) */}
+          <main className='flex-1 h-screen overflow-y-auto p-4 lg:p-10 flex justify-center'>
+            <div className='w-full lg:w-[800px]'>
+              <BlogHeader />
+              {children}
+            </div>
+          </main>
+
+          {/* 데스크톱 사이드바 */}
+          <BlogSidebarClient />
+        </div>
+      </div>
+    </main>
+  );
+}
