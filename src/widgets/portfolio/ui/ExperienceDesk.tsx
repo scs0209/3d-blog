90b6aca879d04dde110d8a0448abcad602737bc1
scs0209 <tjdckdtn2463@naver.com>
2025-07-13 16:40:02 +0900

    feat: 새로운 3D 모델 추가 및 ExperienceDesk 컴포넌트 수정
    - sci-fi_computer_desk_console.glb 파일을 삭제하고 sci_fi_table.glb 파일을 추가하여 3D 모델을 교체
    - ExperienceDesk 컴포넌트에서 useGLTF를 사용하여 새로운 모델을 로드하고, 관련된 메쉬 및 재질을 업데이트하여 시각적 요소 개선

--- src/widgets/portfolio/ui/ExperienceDesk.tsx
+++ src/widgets/portfolio/ui/ExperienceDesk.tsx
@@ -1,36 +1,41 @@
-import { useGLTF } from '@react-three/drei';
 import type * as three from 'three';
+import type { JSX } from 'react';
+import { useGLTF } from '@react-three/drei';
+import type { GLTF } from 'three-stdlib';
 
-export function ExperienceDesk(props: any) {
-  const { nodes, materials } = useGLTF('/sci-fi_computer_desk_console.glb');
+type GLTFResult = GLTF & {
+  nodes: {
+    pPlane7_Tb_0: three.Mesh;
+    pPlane3_Tb_0: three.Mesh;
+    pPlane4_Tb_0: three.Mesh;
+    pPlane8_Tb_0: three.Mesh;
+    pPlane5_Tb_0: three.Mesh;
+    pPlane6_Tb_0: three.Mesh;
+    pCylinder1_Tb_0: three.Mesh;
+  };
+  materials: {
+    material: three.MeshStandardMaterial;
+  };
+};
+
+export function ExperienceDesk(props: JSX.IntrinsicElements['group']) {
+  const { nodes, materials } = useGLTF('/sci_fi_table.glb') as unknown as GLTFResult;
+
   return (
     <group {...props} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
-        <group rotation={[Math.PI / 2, 0, 0]}>
-          <group position={[-39.769, 2.09, -28.414]} rotation={[-1.682, -0.056, 0.342]} scale={[-0.245, 0.1, 0.139]}>
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={(nodes.MaterialFBXASC032FBXASC0352142146907 as three.Mesh)?.geometry}
-              material={materials.MaterialFBXASC032FBXASC0352142146907}
-            />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={(nodes.MaterialFBXASC032FBXASC03511186 as three.Mesh)?.geometry}
-              material={materials.MaterialFBXASC032FBXASC03511186}
-            />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={(nodes.MaterialFBXASC032FBXASC0352142146802 as three.Mesh)?.geometry}
-              material={materials.MaterialFBXASC032FBXASC0352142146802}
-            />
-          </group>
+      <group rotation={[-Math.PI / 2, 0, 0]} scale={15.767}>
+        <group rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+          <mesh castShadow receiveShadow geometry={nodes.pPlane7_Tb_0.geometry} material={materials.material} />
+          <mesh castShadow receiveShadow geometry={nodes.pPlane3_Tb_0.geometry} material={materials.material} />
+          <mesh castShadow receiveShadow geometry={nodes.pPlane4_Tb_0.geometry} material={materials.material} />
+          <mesh castShadow receiveShadow geometry={nodes.pPlane8_Tb_0.geometry} material={materials.material} />
+          <mesh castShadow receiveShadow geometry={nodes.pPlane5_Tb_0.geometry} material={materials.material} />
+          <mesh castShadow receiveShadow geometry={nodes.pPlane6_Tb_0.geometry} material={materials.material} />
+          <mesh castShadow receiveShadow geometry={nodes.pCylinder1_Tb_0.geometry} material={materials.material} />
         </group>
       </group>
     </group>
   );
 }
 
-useGLTF.preload('/sci-fi_computer_desk_console.glb');
+useGLTF.preload('/sci_fi_table.glb');
