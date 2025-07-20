1b78e24295845306ffdf0212f5db8f57457f9b54
scs0209 <tjdckdtn2463@naver.com>
2025-07-20 19:28:22 +0900

    chore: 로그인 및 회원가입 페이지 메타데이터 추가
    - 로그인 페이지에 메타데이터(title, description) 추가
    - 회원가입 페이지에 메타데이터(title, description) 추가
    - 포트폴리오 페이지에서 document.title을 'Portfolio'로 설정하는 useEffect 훅 추가

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -1,4 +1,10 @@
 import { AuthPage } from '@/views/login';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'Login',
+};
 
 const LoginPage = () => {
   return <AuthPage type='login' />;
--- app/(public)/sign-up/page.tsx
+++ app/(public)/sign-up/page.tsx
@@ -1,4 +1,10 @@
 import { AuthPage } from '@/views/login';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Sign Up',
+  description: 'Sign Up',
+};
 
 const RegisterPage = () => {
   return <AuthPage type='signup' />;
