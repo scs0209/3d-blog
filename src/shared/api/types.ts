947ac5a758a9421fd0bf0f58dad49e27f90c6b70
scs0209 <tjdckdtn2463@naver.com>
2025-08-31 18:18:28 +0900

    update: API 요청 파라미터 타입 정의 개선
    - API 요청 파라미터 타입 정의를 개선하여 쿼리 및 경로 파라미터를 보다 명확하게 처리하도록 수정함.
    - RequestQueryParams 및 RequestPathParams 타입을 추가하여 파라미터의 유효성을 높임.

--- src/shared/api/types.ts
+++ src/shared/api/types.ts
@@ -1,5 +1,20 @@
 import type { paths } from './openapi-types';
 
+type Path = keyof paths;
+type Method<P extends Path> = keyof paths[P];
+
+type RequestQueryParams<P extends Path, M extends Method<P>> = paths[P][M] extends {
+  parameters: { query?: unknown };
+}
+  ? paths[P][M]['parameters']['query']
+  : undefined;
+
+type RequestPathParams<P extends Path, M extends Method<P>> = paths[P][M] extends {
+  parameters: { path: unknown };
+}
+  ? paths[P][M]['parameters']['path']
+  : undefined;
+
 export type ApiResponse<T extends keyof paths, M extends keyof paths[T]> = paths[T][M] extends {
   responses: { 200: { content: { 'application/json': infer R } } };
 }
@@ -12,8 +27,6 @@ export type ApiRequest<T extends keyof paths, M extends keyof paths[T]> = paths[
   ? R
   : never;
 
-export type ApiRequestParams<T extends keyof paths, M extends keyof paths[T]> = paths[T][M] extends {
-  parameters: { path: infer R };
-}
-  ? R
-  : never;
+export type ApiRequestParams<T extends keyof paths, M extends keyof paths[T]> =
+  | (RequestQueryParams<T, M> extends undefined ? never : RequestQueryParams<T, M>)
+  | (RequestPathParams<T, M> extends undefined ? never : RequestPathParams<T, M>);
