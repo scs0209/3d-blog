03f720a22480b45a662597df005cf666c35f1c19
scs0209 <tjdckdtn2463@naver.com>
2025-09-03 22:28:35 +0900

    refactor: ProtectLayout 및 AuthSidebar 컴포넌트에서 레이아웃 수정 및 스크롤 영역 추가
    - ProtectLayout에서 SidebarInset 및 ScrollArea를 추가하여 레이아웃을 개선함.
    - AuthSidebar에서 collapsible 및 variant 속성을 추가하여 디자인을 개선함.
    - glassmorphism 효과를 적용하기 위해 스타일을 조정함.

--- app/(protect)/layout.tsx
+++ app/(protect)/layout.tsx
@@ -1,15 +1,26 @@
 import type React from 'react';
-import { SidebarProvider, SidebarTrigger } from '@/shadcn-ui/components/ui/sidebar';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/shadcn-ui/components/ui/sidebar';
 import AuthSidebar from '@/shared/ui/AuthSidebar';
+import { ScrollArea } from '@/shadcn-ui/components/ui/scroll-area';
 
+// TODO: 레이아웃 수정 AuthSidebar가 안에 있어야 glassmorphism 효과를 줄 수 있는데 그러면 안에 컨텐츠랑 겹쳐버림
 const ProtectLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <SidebarProvider>
       <AuthSidebar />
-      <main className='p-4 protect'>
-        <SidebarTrigger />
-        {children}
-      </main>
+      <SidebarInset
+        className='bg-black/40
+    backdrop-blur-2xl
+    border-r border-white/20
+    shadow-[0_8px_32px_rgba(0,0,0,0.4)] h-[800px] p-4'
+      >
+          <ScrollArea className='h-[800px] w-full'>
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+          </ScrollArea>
+      </SidebarInset>
     </SidebarProvider>
   );
 };
--- src/shared/ui/AuthSidebar.tsx
+++ src/shared/ui/AuthSidebar.tsx
@@ -13,7 +13,16 @@ import Link from 'next/link';
 
 const AuthSidebar = () => {
   return (
-    <Sidebar>
+    <Sidebar
+      collapsible='offcanvas'
+      variant='inset'
+      className='fixed left-0 top-0 h-screen w-64
+    bg-black/40
+    backdrop-blur-2xl
+    border-r border-white/20
+    shadow-[0_8px_32px_rgba(0,0,0,0.4)]
+    rounded-r-2xl'
+    >
       <SidebarHeader>
         <h2 className='text-2xl font-bold'>Blog Dashboard</h2>
       </SidebarHeader>
