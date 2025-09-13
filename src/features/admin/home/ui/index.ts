145c1fdc7711f9d40045cc15189da722f447eeef
scs0209 <tjdckdtn2463@naver.com>
2025-09-13 21:02:24 +0900

    feat: 대시보드 카드 컴포넌트 추가
    - 총 게시물 수를 표시하는 `PostsCard` 컴포넌트를 추가함.
    - 총 조회수를 표시하는 `TotalViewsCard` 컴포넌트를 추가함.
    - 대시보드 섹션에 새로운 카드 컴포넌트를 통합하여 사용자, 방문자, 게시물 및 조회수 통계를 시각적으로 표시함.

--- src/features/admin/home/ui/index.ts
+++ src/features/admin/home/ui/index.ts
@@ -1,3 +1,4 @@
 export { VisitorsCard } from './VisitorsCard';
 export { UsersCard } from './UsersCard';
 export { PostsCard } from './PostsCard';
+export { TotalViewsCard } from './TotalViewsCard';
