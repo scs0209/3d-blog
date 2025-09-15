a3a6096882db7edfefa48bac5b8143920438b7e6
scs0209 <tjdckdtn2463@naver.com>
2025-09-15 21:53:12 +0900

    feat: 사용자 업데이트 기능 개선 및 유효성 검사 추가
    - 사용자 업데이트 시 입력 데이터에 대한 유효성 검사를 추가하여 오류를 사전에 방지함.
    - 비밀번호 확인 및 최소 길이 조건을 포함한 스키마를 정의하여 사용자 경험을 향상시킴.
    - 업데이트 후 관리 페이지 리프레시 기능을 추가하여 사용자 편의성을 높임.

--- src/features/user/actions/update-user-action.ts
+++ src/features/user/actions/update-user-action.ts
@@ -2,23 +2,42 @@
 
 import { revalidatePath } from 'next/cache';
 import { updateUser } from '@/features/user/api/user-api';
-import type { Role } from '@prisma/client';
+import { Role } from '@prisma/client';
 import bcrypt from 'bcryptjs';
+import { z } from 'zod';
+
+const userSchema = z
+  .object({
+    id: z.coerce.number(),
+    name: z.string().min(1, 'Name is required.'),
+    email: z.string().email('Invalid email address.'),
+    role: z.nativeEnum(Role),
+    password: z.string().optional(),
+    confirmPassword: z.string().optional(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match.',
+    path: ['confirmPassword'],
+  })
+  .refine((data) => !data.password || data.password.length >= 6, {
+    message: 'Password must be at least 6 characters long.',
+    path: ['password'],
+  });
 
 export async function updateUserAction(prevState: { success: boolean; message: string }, formData: FormData) {
-  const id = Number(formData.get('id'));
-  const name = formData.get('name') as string;
-  const email = formData.get('email') as string;
-  const role = formData.get('role') as Role;
-  const password = formData.get('password') as string;
-  const confirmPassword = formData.get('confirmPassword') as string;
+  const validatedFields = userSchema.safeParse(Object.fromEntries(formData.entries()));
 
+  if (!validatedFields.success) {
+    return {
+      success: false,
+      message: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  const { id, name, email, role, password } = validatedFields.data;
   const updateData: { name: string; email: string; role: Role; password?: string } = { name, email, role };
 
   if (password) {
-    if (password !== confirmPassword) {
-      return { success: false, message: 'Passwords do not match.' };
-    }
     const hashedPassword = await bcrypt.hash(password, 10);
     updateData.password = hashedPassword;
   }
@@ -26,6 +45,7 @@ export async function updateUserAction(prevState: { success: boolean; message: s
   try {
     await updateUser(id, updateData);
     revalidatePath('/admin');
+    revalidatePath(`/admin/users/${id}/edit`);
     return { success: true, message: 'User updated successfully.' };
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
