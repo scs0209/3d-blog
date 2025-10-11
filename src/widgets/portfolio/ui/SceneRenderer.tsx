3d185fd216dec45eecb14055068d9af8889b8cef
scs0209 <tjdckdtn2463@naver.com>
2025-10-11 23:12:28 +0900

    refactor: SceneRenderer에서 'ContactMe' 컴포넌트를 'HomeDoor'로 변경
    - 'contactMe' 관련 참조를 'home'으로 업데이트하여 일관성 유지
    - 'HomeDoor' 컴포넌트의 속성 및 애니메이션 트리거 조정

--- src/widgets/portfolio/ui/SceneRenderer.tsx
+++ src/widgets/portfolio/ui/SceneRenderer.tsx
@@ -3,7 +3,7 @@ import type { FocusedGroup, Position3D } from '@/entities/portfolio/model/types'
 import {
   Antenna,
   Computer,
-  ContactMe,
+  HomeDoor,
   LabMachine,
   ExperienceDesk,
   ExperiencePerson,
@@ -131,19 +131,15 @@ export const SceneRenderer = (props: SceneRendererProps) => {
         ]}
 
       {/* HOME */}
-      {showOtherModels && isShow('contactMe') && (
-        <ContactMe
+      {showOtherModels && isShow('home') && (
+        <HomeDoor
           scale={0.4}
           rotation={[0, -Math.PI / 2, 0]}
           position={[-5, 0, -0.1]}
-          onClick={() => onGroupClick('contactMe')}
+          onClick={() => onGroupClick('home')}
           onPointerOver={handlePointerOver([-5, 0, -0.1])}
           onPointerOut={handlePointerOut}
-          triggerAnimation={focusedGroup === 'contactMe' && cameraAnimationDone}
-          onAnimationComplete={() => {
-            // 홈으로 이동
-            window.location.href = '/';
-          }}
+          triggerAnimation={focusedGroup === 'home' && cameraAnimationDone}
         />
       )}
 
